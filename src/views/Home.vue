4331d5adce0d7ca20bdbd60d447c9d7bad44b255
Anders Lunde <andelu@uio.no>
2022-04-07 13:45:27 +0200

    Changes for BetterReading pilot

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="LANGUAGES - TEST" />
+    <HelloWorld msg="XX LANGUAGES - TEST" />
   </div>
 </template>
 
