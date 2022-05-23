79950e5a8ffa24edf91b365fa84f0a12ff111956
Anders Lunde <andelu@uio.no>
2022-05-23 13:03:55 +0200

    Developing...

--- src/components/DataPresentation.vue
+++ src/components/DataPresentation.vue
@@ -164,7 +164,7 @@ export default Vue.extend({
         if (response.status == 200) {
           arrayOfjsons = await response.json();
         } else {
-          alert(response.status, "Error: possibly wrong password");
+          alert(response.status + "Error: possibly wrong password");
         }
       } catch (err) {
         console.log("Fetch Error: ", err);
